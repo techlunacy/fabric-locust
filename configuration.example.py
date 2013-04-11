57f31aa916bca7e06c2b2cb173a7fb9e2ca30956
Graeme <graeme.wilson@iplayup.com>
2013-04-11 15:46:11 +1000

    the configuration example goodness

---
+++ configuration.example.py
@@ -0,0 +1,22 @@
+__author__ = 'Graeme Wilson'
+
+# AWS KEY SETTINGS
+AWS_API_KEY = 'AWS ACCESS KEY'
+AWS_SECRET_KEY = 'AWS SECRET ACCESS KEY'
+AWS_KEY_FILE = 'cloudformation-us-west-2'
+LOCAL_AWS_KEY_FILE = 'key for logging into the instances'
+
+AMI_SECURITY_GROUPS = ['security groups..... should have these created automatically if they are needed']
+AWS_USER_NAME = 'ubuntu'
+AWS_INSTANCE_SIZE = 't1.micro'
+
+PROJECT = "TEST"
+LOCAL_DIRECTORY = 'path to test directory'
+LOCAL_LOCUST_FILE = 'locustfile.py'
+REQUIREMENTS_FILE = 'requirements.pip'
+
+AWS_REGION = 'us-west-2'
+AMI_ID = 'ubuntu 12.04 image'
+SLAVE_COUNT = 3
+
+TARGET_DOMAIN = 'http://example.com'
